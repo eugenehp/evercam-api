5bc61c16856792fb6100ebefe4e594068834bfb5
Eugene HP <eugene.hp2012@gmail.com>
2015-08-16 13:09:50 +0200

    changed indentation

--- migrations/20150813143400_add_motion_detection_in_snapshots_table.rb
+++ migrations/20150813143400_add_motion_detection_in_snapshots_table.rb
@@ -1,16 +1,12 @@
 Sequel.migration do
-
   up do
-
     alter_table(:snapshots) do
       add_column :motiondetection, :integer, null: true
     end
-
   end
 
   down do
     alter_table(:snapshots)
       drop_column :motiondetection
   end
-
 end
